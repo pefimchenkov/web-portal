e4da04b155896b8ca031f2f9ffa09b250f953103
Павел Ефимченков (223) <epf@tsd-group.ru>
2020-01-10 13:24:21 +0300

    hide config fb

--- src/config/fb.js
+++ src/config/fb.js
@@ -1,8 +1,8 @@
 module.exports = {
-	apiKey: 'AIzaSyDl2M-tIePSginA3hzMVChNZWadj0V6FEk',
-	authDomain: 'web-portal-75244.firebaseapp.com',
-	databaseURL: 'https://web-portal-75244.firebaseio.com',
-	projectId: 'web-portal-75244',
-	storageBucket: 'web-portal-75244.appspot.com',
-	messagingSenderId: '804378274290'
+	apiKey: 'xxx',
+	authDomain: 'xxx',
+	databaseURL: 'xxx',
+	projectId: 'xxx',
+	storageBucket: 'xxx',
+	messagingSenderId: 'xxx'
 }
